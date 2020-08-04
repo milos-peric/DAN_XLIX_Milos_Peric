f98db85caa7820486bf102c5de77272d2f38dce1
Milos Peric <steamestt@gmail.com>
2020-08-04 15:37:02 +0200

    +Add data base service

---
+++ DAN_XLIX_Milos_Peric/DAN_XLIX_Milos_Peric/ViewModel/AddEmployeeViewModel.cs
@@ -0,0 +1,54 @@
+﻿using DAN_XLIX_Milos_Peric.Command;
+using DAN_XLIX_Milos_Peric.View;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace DAN_XLIX_Milos_Peric.ViewModel
+{
+    class AddEmployeeViewModel : ViewModelBase
+    {
+        AddEmployeeView employee;
+        public AddEmployeeViewModel(AddEmployeeView viewEmployee)
+        {
+            employee = viewEmployee;
+        }
+
+        private ICommand cancelCommand;
+        public ICommand CancelCommand
+        {
+            get
+            {
+                if (cancelCommand == null)
+                {
+                    cancelCommand = new RelayCommand(param => CancelCommandExecute());
+                }
+                return cancelCommand;
+            }
+        }
+
+        private void CancelCommandExecute()
+        {
+            try
+            {
+                MessageBoxResult result = MessageBox.Show("Are you sure you want to close window?", "Close Window", MessageBoxButton.YesNo);
+                switch (result)
+                {
+                    case MessageBoxResult.Yes:
+                        employee.Close();
+                        break;
+                    case MessageBoxResult.No:
+                        break;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+    }
+}
---
+++ DAN_XLIX_Milos_Peric/DAN_XLIX_Milos_Peric/ViewModel/AddManagerViewModel.cs
@@ -0,0 +1,54 @@
+﻿using DAN_XLIX_Milos_Peric.Command;
+using DAN_XLIX_Milos_Peric.View;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace DAN_XLIX_Milos_Peric.ViewModel
+{
+    class AddManagerViewModel : ViewModelBase
+    {
+        AddManagerView manager;
+        public AddManagerViewModel(AddManagerView viewManager)
+        {
+            manager = viewManager;
+        }
+
+        private ICommand cancelCommand;
+        public ICommand CancelCommand
+        {
+            get
+            {
+                if (cancelCommand == null)
+                {
+                    cancelCommand = new RelayCommand(param => CancelCommandExecute());
+                }
+                return cancelCommand;
+            }
+        }
+
+        private void CancelCommandExecute()
+        {
+            try
+            {
+                MessageBoxResult result = MessageBox.Show("Are you sure you want to close window?", "Close Window", MessageBoxButton.YesNo);
+                switch (result)
+                {
+                    case MessageBoxResult.Yes:
+                        manager.Close();
+                        break;
+                    case MessageBoxResult.No:
+                        break;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+    }
+}
